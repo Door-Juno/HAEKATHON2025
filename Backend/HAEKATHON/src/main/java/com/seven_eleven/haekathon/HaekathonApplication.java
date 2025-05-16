8be8493e63268b68d70a27b4ad6ddafaa9a344dc
Door_juno <bublman3375@knu.ac.kr>
2025-05-17 01:50:47 +0900

    init : spring backend setup

---
+++ Backend/HAEKATHON/src/main/java/com/seven_eleven/haekathon/HaekathonApplication.java
@@ -0,0 +1,13 @@
+package com.seven_eleven.haekathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaekathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaekathonApplication.class, args);
+    }
+
+}
---
+++ Backend/HAEKATHON/src/test/java/com/seven_eleven/haekathon/HaekathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seven_eleven.haekathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaekathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
